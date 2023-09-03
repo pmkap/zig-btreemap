afa45d674a045f6873a694fb86f8217437e00709
Ganesan Rajagopal <rganesan@gmail.com>
2023-09-03 14:34:10 +0530

    zig 0.11 compatibility fixes
    Zig 0.11 has lot of breaking changes to build.zig including native
    support for modules. btreemap is now a module that can be imported
    using a build.zig.zon file.

--- build.zig
+++ build.zig
@@ -1,17 +1,24 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
-    // Standard release options allow the person running `zig build` to select
+pub fn build(b: *std.build) void {
+    // Standard optimize option allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
 
-    const lib = b.addStaticLibrary("zig-btreemap", "src/btreemap.zig");
-    lib.setBuildMode(mode);
-    lib.install();
+    const btreemap_module = b.createModule(.{ //
+        .source_file = .{ .path = "src/btreemap.zig" },
+    });
 
-    const main_tests = b.addTest("src/btreemap.zig");
-    main_tests.setBuildMode(mode);
+    const main_tests = b.addTest(.{ //
+        .name = "zig btreemap tests",
+        .root_source_file = .{ .path = "src/btreemap.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    main_tests.addModule("btreemap", btreemap_module);
+    const run_main_tests = b.addRunArtifact(main_tests);
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&run_main_tests.step);
 }
